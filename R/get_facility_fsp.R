e864c0f863d5a57b3a2d2a97fcbac0fb6f41d530
Sophie Hoffman <shoffm@umich.edu>
2021-03-02 13:21:29 +0000

    add tree subset and functionalize facility fsp

--- R/get_facility_fsp.R
+++ R/get_facility_fsp.R
@@ -22,76 +22,58 @@
 get_facility_fsp <- function(fasta, locs){
   #check the DNAbin object and locs
   check_facility_fsp(fasta, locs)
-  #TODO: add check to make sure at least 2 of the locs appear more than once
   #make a vector of only locs that appear more than once
   locs_over_one <- which(unlist(table(locs) > 1))
   locs_subset <- locs[locs %in% names(locs_over_one)]
   #make a list of the ones they have in common for subsetting
   isolates <- intersect(names(locs_subset), rownames(fasta))
   #subset the DNAbin object to the samples they have in common
-  fasta<-fasta[isolates,]
+  fasta_sub<-fasta[isolates,]
   #subset the locs object to the samples they have in common
   locs_subset <- locs_subset[isolates]
   #order the locs object to match the order of rownames of the fasta
-  locs_subset <- locs_subset[order(match(names(locs_subset),rownames(fasta)))]
+  #this might be redundant
+  locs_subset <- locs_subset[order(match(names(locs_subset),rownames(fasta_sub)))]
   #change the rownames of the fasta to the location names
-  rownames(fasta) <- unname(locs_subset)
+  rownames(fasta_sub) <- unname(locs_subset)
   #make a list of the location names
-  sample_locs = rownames(fasta)
+  sample_locs <- rownames(fasta_sub)
   #unique list of locations
-  locs_unique = unique(unname(locs_subset))
+  locs_unique <- unique(unname(locs_subset))
 
   #CALCULATE INTRA- AND INTER-FACILITY DISTANCE
   facil_dist <- data.frame(sapply(locs_unique, function(f1){
     sapply(locs_unique, function(f2){
       if (f1 == f2) {return(0)}
       #subset fasta file to just that those locations
-      subset_snp_mat = fasta[sample_locs %in% c(f1, f2), ]
+      subset_snp_mat = fasta_sub[sample_locs %in% c(f1, f2), ]
       #make sure the position is not all unknown or no variance, subset to the ones that have some variation
       subset_snp_mat = subset_snp_mat[,apply(subset_snp_mat, 2, FUN = function(x){sum(x != x[1] | x == 'N') > 0})]
       #figure out which are from each facility
       subset_f1 = rownames(subset_snp_mat) %in% f1
       subset_f2 = rownames(subset_snp_mat) %in% f2
       #BETWEEN POPLUATION VARIATION
-      #loop over columns (variants)
+      #for each position
       between = apply(subset_snp_mat, 2, FUN = function(x){
         #get alleles present at the site
         alleles = names(table(as.character(x)))
         #skip multi-allelic sites
         if (length(alleles) > 2){0} else{
           #find allele frequency for each allele at each site
-          #make function to call x4
-          f1_allele1 = sum(as.character(x)[subset_f1] %in% alleles[1])/sum(subset_f1)
-          f1_allele2 = sum(as.character(x)[subset_f1] %in% alleles[2])/sum(subset_f1)
-          f2_allele1 = sum(as.character(x)[subset_f2] %in% alleles[1])/sum(subset_f2)
-          f2_allele2 = sum(as.character(x)[subset_f2] %in% alleles[2])/sum(subset_f2)
+          f1_allele1 = allele_freq_btwn(x, subset_f1, 1)
+          f1_allele2 = allele_freq_btwn(x, subset_f1, 2)
+          f2_allele1 = allele_freq_btwn(x, subset_f2, 1)
+          f2_allele2 = allele_freq_btwn(x, subset_f2, 2)
+          #calculate between pop variation for each allele site?
           f1_allele1 * f1_allele2 * f2_allele1 * f2_allele2}
       })
       #sum
       between_sum = sum(between)
       #WITHIN POPULATION 1 VARIATION
-      #subset to first location
-      f1_subset_snp_mat = subset_snp_mat[subset_f1,apply(subset_snp_mat[subset_f1,], 2, FUN = function(x){sum(x != x[1] | x == 'N') > 0})]
-      #same as above
-      within_f1 = apply(f1_subset_snp_mat, 2, FUN = function(x){
-        alleles = names(table(as.character(x)))
-        if (length(alleles) > 2){0}else{
-          f1_allele1 = sum(as.character(x) %in% alleles[1])/length(x)
-          f1_allele2 = sum(as.character(x) %in% alleles[2])/length(x)
-          (f1_allele1 * f1_allele2)^2
-        }
-      })
-      within_f1_sum = sum(within_f1)
+      within_f1_sum <- within_pop_var(subset_snp_mat, subset_f1)
       #WITHIN POPULATION 2 VARIATION
-      f2_subset_snp_mat = subset_snp_mat[subset_f2,apply(subset_snp_mat[subset_f2,], 2, FUN = function(x){sum(x != x[1] | x == 'N') > 0})]
-      within_f2 = apply(f2_subset_snp_mat, 2, FUN = function(x){
-        alleles = names(table(as.character(x)))
-        if (length(alleles) > 2){0}else{
-          f2_allele1 = sum(as.character(x) %in% alleles[1])/length(x)
-          f2_allele2 = sum(as.character(x) %in% alleles[2])/length(x)
-          (f2_allele1 * f2_allele2)^2}
-      })
-      within_f2_sum = sum(within_f2)
+      within_f2_sum <- within_pop_var(subset_snp_mat, subset_f2)
+      #calculate Fsp
       Fsp = (((within_f1_sum + within_f2_sum) / 2) - between_sum) / ((within_f1_sum + within_f2_sum) / 2)
       return(Fsp)
     })#end loop 1
@@ -103,6 +85,121 @@ get_facility_fsp <- function(fasta, locs){
 }#end facility_fst
 
 
+#find allele frequency for each allele at each site
+allele_freq_btwn <- function(x, subset, allele_n){
+  return(sum(as.character(x)[subset] %in% alleles[allele_n])/sum(subset))
+}
+
+allele_freq_within <- function(x, allele_n){
+  return(sum(as.character(x) %in% alleles[allele_n])/length(x))
+}
+
+within_pop_var <- function(subset_snp_mat, subset){
+  f_subset_snp_mat = subset_snp_mat[subset,apply(subset_snp_mat[subset,], 2, FUN = function(x){sum(x != x[1] | x == 'N') > 0})]
+  within_f = apply(f_subset_snp_mat, 2, FUN = function(x){
+    alleles = names(table(as.character(x)))
+    if (length(alleles) > 2){0}else{
+      f_allele1 = allele_freq_within(x, 1)
+      f_allele2 = allele_freq_within(x, 2)
+
+      (f_allele1 * f_allele2)^2}
+  })
+  return(sum(within_f))
+}
+
+########################sapply no funcs########
+# get_facility_fsp <- function(fasta, locs){
+#   #check the DNAbin object and locs
+#   check_facility_fsp(fasta, locs)
+#   #make a vector of only locs that appear more than once
+#   locs_over_one <- which(unlist(table(locs) > 1))
+#   locs_subset <- locs[locs %in% names(locs_over_one)]
+#   #make a list of the ones they have in common for subsetting
+#   isolates <- intersect(names(locs_subset), rownames(fasta))
+#   #subset the DNAbin object to the samples they have in common
+#   fasta<-fasta[isolates,]
+#   #subset the locs object to the samples they have in common
+#   locs_subset <- locs_subset[isolates]
+#   #order the locs object to match the order of rownames of the fasta
+#   locs_subset <- locs_subset[order(match(names(locs_subset),rownames(fasta)))]
+#   #change the rownames of the fasta to the location names
+#   rownames(fasta) <- unname(locs_subset)
+#   #make a list of the location names
+#   sample_locs = rownames(fasta)
+#   #unique list of locations
+#   locs_unique = unique(unname(locs_subset))
+#
+#   #CALCULATE INTRA- AND INTER-FACILITY DISTANCE
+#   facil_dist <- data.frame(sapply(locs_unique, function(f1){
+#     sapply(locs_unique, function(f2){
+#       if (f1 == f2) {return(0)}
+#       #subset fasta file to just that those locations
+#       subset_snp_mat = fasta[sample_locs %in% c(f1, f2), ]
+#       #make sure the position is not all unknown or no variance, subset to the ones that have some variation
+#       subset_snp_mat = subset_snp_mat[,apply(subset_snp_mat, 2, FUN = function(x){sum(x != x[1] | x == 'N') > 0})]
+#       #figure out which are from each facility
+#       subset_f1 = rownames(subset_snp_mat) %in% f1
+#       subset_f2 = rownames(subset_snp_mat) %in% f2
+#       #BETWEEN POPLUATION VARIATION
+#       #loop over columns (variants)
+#       between = apply(subset_snp_mat, 2, FUN = function(x){
+#         #get alleles present at the site
+#         alleles = names(table(as.character(x)))
+#         #skip multi-allelic sites
+#         if (length(alleles) > 2){0} else{
+#           #find allele frequency for each allele at each site
+#           #make function to call x4
+#           # f1_allele1 = sum(as.character(x)[subset_f1] %in% alleles[1])/sum(subset_f1)
+#           # f1_allele2 = sum(as.character(x)[subset_f1] %in% alleles[2])/sum(subset_f1)
+#           # f2_allele1 = sum(as.character(x)[subset_f2] %in% alleles[1])/sum(subset_f2)
+#           # f2_allele2 = sum(as.character(x)[subset_f2] %in% alleles[2])/sum(subset_f2)
+#           f1_allele1 = allele_freq_btwn(x, subset_f1, 1)
+#           f1_allele2 = allele_freq_btwn(x, subset_f1, 2)
+#           f2_allele1 = allele_freq_btwn(x, subset_f2, 1)
+#           f2_allele2 = allele_freq_btwn(x, subset_f2, 2)
+#           f1_allele1 * f1_allele2 * f2_allele1 * f2_allele2}
+#       })
+#       #sum
+#       between_sum = sum(between)
+#       #WITHIN POPULATION 1 VARIATION
+#       within_f1_sum <- within_pop_var(subset_snp_mat, subset_f1)
+#       # #subset to first location
+#       # f1_subset_snp_mat = subset_snp_mat[subset_f1,apply(subset_snp_mat[subset_f1,], 2, FUN = function(x){sum(x != x[1] | x == 'N') > 0})]
+#       # #same as above
+#       # within_f1 = apply(f1_subset_snp_mat, 2, FUN = function(x){
+#       #   alleles = names(table(as.character(x)))
+#       #   if (length(alleles) > 2){0}else{
+#       #     # f1_allele1 = sum(as.character(x) %in% alleles[1])/length(x)
+#       #     # f1_allele2 = sum(as.character(x) %in% alleles[2])/length(x)
+#       #     f1_allele1 = allele_freq_within(x, 1)
+#       #     f1_allele2 = allele_freq_within(x, 2)
+#       #     (f1_allele1 * f1_allele2)^2
+#       #   }
+#       # })
+#       # within_f1_sum = sum(within_f1)
+#       #WITHIN POPULATION 2 VARIATION
+#       within_f2_sum <- within_pop_var(subset_snp_mat, subset_f2)
+#       # f2_subset_snp_mat = subset_snp_mat[subset_f2,apply(subset_snp_mat[subset_f2,], 2, FUN = function(x){sum(x != x[1] | x == 'N') > 0})]
+#       # within_f2 = apply(f2_subset_snp_mat, 2, FUN = function(x){
+#       #   alleles = names(table(as.character(x)))
+#       #   if (length(alleles) > 2){0}else{
+#       #     # f2_allele1 = sum(as.character(x) %in% alleles[1])/length(x)
+#       #     # f2_allele2 = sum(as.character(x) %in% alleles[2])/length(x)
+#       #     f2_allele1 = allele_freq_within(x, 1)
+#       #     f2_allele2 = allele_freq_within(x, 2)
+#       #
+#       #     (f2_allele1 * f2_allele2)^2}
+#       # })
+#       # within_f2_sum = sum(within_f2)
+#       Fsp = (((within_f1_sum + within_f2_sum) / 2) - between_sum) / ((within_f1_sum + within_f2_sum) / 2)
+#       return(Fsp)
+#     })#end loop 1
+#   }))#end loop 2
+#   #add row and column names
+#   rownames(facil_dist) <- locs_unique
+#   colnames(facil_dist) <- locs_unique
+#   return(facil_dist);
+# }#end facility_fst
 ########################3normal!#############
 # get_facility_fsp <- function(fasta, locs){
 #   #check the DNAbin object and locs
--- extras/Make_data_subset.R
+++ extras/Make_data_subset.R
@@ -1,4 +1,4 @@
-#subset Penn dataset for github 
+#subset Penn dataset for github
 library(ape)
 
 #load big data files ###############
@@ -14,7 +14,7 @@ fasta <- ape::read.dna("/Users/sophiehoffman/Desktop/gl_mount/Project_Penn_KPC/S
 
 #tree path Penn
 #/nfs/turbo/umms-esnitkin/Project_Penn_KPC/Sequence_data/2021_02_10_Penn_All_variant_calling/2021_02_12_08_34_28_core_results/gubbins/iqtree_masked_wga/2021_02_12_08_34_28_KPNIH1_genome_aln_w_alt_allele_unmapped.treefile
-tree <- ape::read.tree("/Users/sophiehoffman/Desktop/gl_mount/Project_Penn_KPC/Sequence_data/2021_02_10_Penn_All_variant_calling/2021_02_12_08_34_28_core_results/gubbins/iqtree_masked_wga/2021_02_12_08_34_28_KPNIH1_genome_aln_w_alt_allele_unmapped.treefile")
+tr <- ape::read.tree("/Users/sophiehoffman/Desktop/gl_mount/Project_Penn_KPC/Sequence_data/2021_02_10_Penn_All_variant_calling/2021_02_12_08_34_28_core_results/gubbins/iqtree_masked_wga/2021_02_12_08_34_28_KPNIH1_genome_aln_w_alt_allele_unmapped.treefile")
 
 #locs - locations of isolates (e.g. facility of isolation)
 #named vector, where names are same as names of dist.dna output
@@ -25,26 +25,34 @@ names(locs) <- paste0("PCMP_H", metadata$isolate_no)
 pt <- metadata$patient_id
 names(pt) <- paste0("PCMP_H", metadata$isolate_no)
 
-#subset big data files 
+#subset big data files
 locs_sub <- locs[1:102]
 pt_sub <- pt[1:102]
 common <- intersect(names(locs_sub), rownames(fasta))
 locs_sub <- locs[names(locs) %in% common]
 pt_sub <- pt[names(pt) %in% common]
 fasta_sub<-fasta[common,]
+#subset the tree using keep.tip
+tr_subset <- keep.tip(tr,names(locs))
 
 dists <- dist.dna(x = fasta_sub, as.matrix = TRUE, model = "N")
 
-#make list 
-Penn_test_input <- list("locs" = locs_sub, "pt" = pt_sub, "fasta" = fasta_sub, "dists" = dists)
+#make list
+#Penn_test_input <- list("locs" = locs_sub, "pt" = pt_sub, "fasta" = fasta_sub, "dists" = dists)
+Penn_test_input <- list("locs" = locs, "pt" = pt, "fasta" = fasta, "dists" = dists, "tr" = tr_subset)
 
 getwd()
 setwd("/Users/sophiehoffman/Desktop/regentrans/extras")
-saveRDS(Penn_test_input, file = "Penn_test_input.rds")
+saveRDS(Penn_test_input, file = "Penn_test_input_2.rds")
 
-#test read back in 
-Penn_test_input <- readRDS(file = "/Users/sophiehoffman/Desktop/regentrans/extras/Penn_test_input.rds")
+#test read back in
+Penn_test_input <- readRDS(file = "/Users/sophiehoffman/Desktop/regentrans/extras/Penn_test_input_2.rds")
 locs <- Penn_test_input$locs
 pt <- Penn_test_input$pt
 fasta <- Penn_test_input$fasta
 dists <- Penn_test_input$dists
+tr <- Penn_test_input$tr
+
+
+
+
