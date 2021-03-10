2c116a600d3ffc138903589ac709e67c426b8a18
Sophie Hoffman <shoffm@umich.edu>
2021-03-10 16:09:01 +0000

    finish checks tests and add test for get_snv_dists output

--- R/get_snv_dists.R
+++ R/get_snv_dists.R
@@ -1,24 +1,3 @@
-# some notes:
-# we might want to change the function names (should start with a verb, be informative and not too long)
-# we might want to change the format/colnames of the output as well
-# good to include checks to see if the input is correct (within the function; can have a separate script for all check functions) and unit tests to check the functions (also in a separate script; we'll hopefully find bugs this way!)
-# we'll want to make some arguments optional - i've tried to note where this will be, because they're not always optional now
-# we'll want to be consistent with what arguments are called across functions (same name = same input)
-# you'll have to put the package name in front of functions that come from a different package
-
-# compare pairwise snv distances returned by ape::dist.dna() using pairwise.deletion = T and F
-# example of using dist.dna: dist.dna(dna_aln, model = 'N', pairwise.deletion = T, as.matrix = T)
-
-# get intra- and inter-facility pairwise snv distances (might want to change this name to explain better what we're outputting)
-# dists - snv distance matrix returned by dist.dna
-# should we run dist.dna in here? I guess it depends whether any other functions need that as input, then it would be in its own prep function?
-# locs - locations of isolates (e.g. facility of isolation)
-# pt - patient isolate was taken from (optional; will remove pairwise snv distances between the same patient)
-
-
-
-
-
 #' Get matrix of inter- and intra-facility pariwise snv distances
 #'
 #' @param dists a SNV distance matrix returned by the dist.dna function from the ape package
@@ -72,7 +51,7 @@ get_snv_dists <- function(dists, locs, pt = NULL){
   }
   else{
     #add labels
-    snp_facility_pairs <- bind_cols(snps %>% mutate(Pair_Type=ifelse(Loc1==Loc2,'Intra-facility pair','Inter-facility pair')))
+    snp_facility_pairs <- bind_cols(snps %>% filter(Isolate1 != Isolate2) %>% mutate(Pair_Type=ifelse(Loc1==Loc2,'Intra-facility pair','Inter-facility pair')))
   }
   #return snp matrix
   return(snp_facility_pairs)
