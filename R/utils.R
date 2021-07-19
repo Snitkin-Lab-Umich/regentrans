fddbd4393aeac654b97ebcba48ee83e6975ad5ca
Zena Lapp <zenalapp@gmail.com>
2021-07-19 15:58:45 -0400

    Export only user-facing functions resolves #32

--- R/utils.R
+++ R/utils.R
@@ -1,9 +1,30 @@
-#utils 
+#utils
 #' dplyr pipe
 #' @importFrom dplyr %>%
-#' @export
+#' @noRd
 dplyr::`%>%`
 
 ## make R CMD CHECK shut up about the dot `.``
 ## See: \url{https://github.com/tidyverse/magrittr/issues/29}
 utils::globalVariables(c("."))
+
+#' Reverse list structure
+#'
+#' @param ls list you want to reverse
+#'
+#' @return reversed list
+#' @noRd
+#'
+#' @examples
+#' #Reference with example: https://stackoverflow.com/questions/15263146/revert-list-structure
+reverse_list_str <- function(ls) { # @Josh O'Brien
+  #checks
+  check_reverse_list_str_input(ls)
+
+  # get sub-elements in same order
+  #x <- future.apply::future_lapply(ls, `[`, names(ls[[1]]))
+  x <- lapply(ls, `[`, names(ls[[1]]))
+  # stack and reslice
+  #future.apply::future_apply(do.call(rbind, x), 2, as.list)
+  apply(do.call(rbind, x), 2, as.list)
+}
