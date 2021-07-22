bb9e6cba244dd4bc5e57386e441b0a43021b2c1c
Zena Lapp <zenalapp@gmail.com>
2021-07-22 15:54:58 -0400

    Update vignette

--- tests/testthat/test-summarize_pairs.R
+++ tests/testthat/test-summarize_pairs.R
@@ -1,12 +1,12 @@
-# tests for summarize_inter_pairs
+# tests for summarize_pairs
 
 locs <- metadata %>% dplyr::select(isolate_id, facility) %>% tibble::deframe()
 test_locs <- locs[1:3]
 test_dists <- dists[names(test_locs), names(test_locs)]
 test_snv_dists <- get_snv_dists(dists = test_dists, locs = test_locs)
-test_inter_pair_summary <- summarize_inter_pairs(test_snv_dists, summary_fns = c("min"), threshs = c(20, 50))
-test_inter_pair_summary_2 <- summarize_inter_pairs(test_snv_dists, summary_fns = c("min"), threshs = NULL)
-test_inter_pair_summary_3 <- summarize_inter_pairs(test_snv_dists, summary_fns = NULL, threshs = c(20, 50))
+test_inter_pair_summary <- summarize_pairs(test_snv_dists, summary_fns = c("min"), threshs = c(20, 50))
+test_inter_pair_summary_2 <- summarize_pairs(test_snv_dists, summary_fns = c("min"), threshs = NULL)
+test_inter_pair_summary_3 <- summarize_pairs(test_snv_dists, summary_fns = NULL, threshs = c(20, 50))
 test_pt_flow <- pt_trans_df %>% dplyr::filter(source_facil %in% c('C','D','E') & dest_facil %in% c('C','D','E')) %>% get_patient_flow()
 test_fsp_long <- make_long_form(fsp)[1:2,]
 merged_summaries <- merge_inter_summaries(test_pt_flow, test_inter_pair_summary, test_fsp_long)
@@ -14,7 +14,7 @@ merged_summaries_2 <-  merge_inter_summaries(NULL, test_inter_pair_summary, test
 merged_summaries_3 <- merge_inter_summaries(test_pt_flow, NULL, test_fsp_long)
 merged_summaries_4 <- merge_inter_summaries(test_pt_flow, test_inter_pair_summary, NULL)
 
-test_that("summarize_inter_pairs works", {
+test_that("summarize_pairs works", {
   # everything
   expect_equal(test_inter_pair_summary,
                structure(list(Loc1 = c("A", "A", "B"), Loc2 = c("B", "C", "C"
